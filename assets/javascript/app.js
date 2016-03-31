9c587cf356f19c6b04b0d3a9cf26498f0afe9b8a
kellymersereau <kelly.mersereau@gmail.com>
2016-03-31 19:50:58 -0400

    finalized some stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,106 +1,110 @@
-var pos = "";
-var test = '';
+var userInput = 
 
-// $('#submitButton').on('click', function(){
-//   function createArtistList(){
-//     var artist = $('<li>');
-//         i++;
-//         artist.attr('data-num', [i])
-//   }
-// })
+$('#submitButton').on('click', function(){
+  userInput= $("#userInput").val().trim();
+  createArtistList();
+  $('#userInput').val("");
+  return false;
+})
+
+function createArtistList(){
+  var artist = $('<li>');
+      artist.addClass('user_input');
+      artist.text(userInput);
+      $(".userList").append(artist);
+}
 
 // FACEBOOK LOGIN API
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
-      console.log('statusChangeCallback');
-      console.log(response);
-      // The response object is returned with a status field that lets the app know the current login status of the person. Full docs on the response object can be found in the documentation for FB.getLoginStatus().
-      if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        testAPI();
-      } else if (response.status === 'not_authorized') {
-        // The person is logged into Facebook, but not your app.
-        document.getElementById('status').innerHTML = 'Please log ' +
-          'into this app.';
-      } else {
-           // The person is not logged into Facebook, so we're not sure if hey are logged into this app or not.
-           document.getElementById('status').innerHTML = 'Please log ' +
-             'into Facebook.';
-      }
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the app know the current login status of the person. Full docs on the response object can be found in the documentation for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if hey are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
+    }
   };
   // This function is called when someone finishes with the Login Button.  See the onlogin handler attached to it in the sample code below.
-      function checkLoginState() {
-         	FB.getLoginStatus(function(response) {
-           	statusChangeCallback(response);
-         	});
-      };
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  };
 
   window.fbAsyncInit = function() {
-       FB.init({
-         appId      : '1729912430587543',
-         cookie     : true,  // enable cookies to allow the server to access 
-                             // the session
-         xfbml      : true,  // parse social plugins on this page
-         version    : 'v2.5' // use graph api version 2.5
-      });
-  // Now that we've initialized the JavaScript SDK, we call FB.getLoginStatus().  This function gets the state of the person visiting this page and can return one of three states to the callback you provide.  They can be:
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into your app or not.
-    // These three cases are handled in the callback function.
+    FB.init({
+      appId      : '1729912430587543',
+      cookie     : true,  // enable cookies to allow the server to access the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.5' // use graph api version 2.5
+    });
+    // Now that we've initialized the JavaScript SDK, we call FB.getLoginStatus().  This function gets the state of the person visiting this page and can return one of three states to the callback you provide.  They can be:
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into your app or not.
+      // These three cases are handled in the callback function.
 
-      FB.getLoginStatus(function(response) {
-    	    statusChangeCallback(response);
-      });
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
   };
 
 
   // Load the SDK asynchronously
   (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
   // Here we run a very simple test of the Graph API after login is successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
-      console.log('Welcome!  Fetching your information.... ');
-     	FB.api('/me', function(response) {
-          console.log('Successful login for: ' + response.name);
-          document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
-      });
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+    });
   };
   function getUserInfo(){
-  	FB.login(function(response) {
-  	  // handle the response
-  	}, {scope: 'user_likes, user_actions.music'});
-  	FB.api(
-  	    '/me',
-  	    'GET',
-  	    {"fields":"music{genre,artists_we_like,name, picture{url}}"},
-  	    function(response) {
-  	        console.log(response);
-          }
-      );
+    FB.login(function(response) {
+      // handle the response
+    }, {scope: 'user_likes, user_actions.music'});
+    FB.api(
+      '/me',
+      'GET',
+      {"fields":"music{genre,artists_we_like,name, picture{url}}"},
+      function(response) {
+        console.log(response);
+      }
+    );
   };
 
-
 //GOOGLE MAPS SCRIPT
+  // global variable for position. this will end up storingthe user's position
+  var pos = "";
+
   var map;
   //used to name the markers that will appear when user clicks
   var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var labelIndex = 0;
 
-  //creates my center on the map
-  myCenter = new google.maps.LatLng(40.6, -74);
+  // //creates my center on the map
+  // myCenter = new google.maps.LatLng(40.6, -74);
 
   //initializes google map
-  function initialize() {
+  function initMap() {
     var brooklyn = {lat: +40.6, lng: -74},
     map = new google.maps.Map(document.getElementById('googleMap'), {
       center: {lat: +40.663, lng: -73.982},
-        zoom: 14
+      zoom: 10
     });
     //info window that shows current location
     var infoWindow = new google.maps.InfoWindow({map: map});
@@ -114,9 +118,9 @@ var test = '';
 
     function toggleBounce() {
       if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
+        marker.setAnimation(null);
       } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
       }
     }
     var contentString = '<div id="content">'+
@@ -124,16 +128,7 @@ var test = '';
       '</div>'+
       '<h1 id="firstHeading" class="firstHeading">Band Name or Header</h1>'+
       '<div id="bodyContent">'+
-      '<p><b>Content</b>, also <b>more content</b>, is a content ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
+      '<p><b>Content</b>, also <b>more content</b>, is a content '
       '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
       'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
       '(last visited June 22, 2009).</p>'+
@@ -153,87 +148,187 @@ var test = '';
       addMarker(event.latLng, map);
     });
 
-    // Add a marker at the center of the map.
-    addMarker(brooklyn, map);  
+    // // Add a marker at the center of the map.
+    // addMarker(brooklyn, map);  
 
     // Adds a marker to the map.
     function addMarker(location, map) {
       // Add the marker at the clicked location, and add the next-available label from the array of alphabetical characters.
-      var marker = new google.maps.Marker({
-        position: location,
-        label: labels[labelIndex++ % labels.length],
-        map: map
-      });
-    }
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        map.setCenter(pos);
-      }, 
-      function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+      for (var i = 0; i < bitResponse.length; i++) {
+        var venueData = bitResponse[i].venue;
+        var showData = bitResponse[i];
+        var myLatlng = new google.maps.LatLng(venueData.latitude, venueData.longitude);
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          label: bitResponse.title
+        });
       
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
+      //Attach click event to the marker.
+      (function (marker, venueData) {
+        google.maps.event.addListener(marker, "click",function (e) {
+          //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+          infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" +showData.title + "</div>");
+          infoWindow.open(map, marker);
+        });
+      })(marker, venueData);
     }
   }
 
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, 
+    function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+      
+  function handleLocationError(browserHasGeolocation,infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  }
+}
+
 // BANDS IN TOWN API
 
-var bands = [];
-var venueLatLon = [];
-var venueNames = [];
-var bitResponse = '';
+
+
+// the following two functions work, but for some reason, they only work after I put addToArray() in the conosle. they don't work automatically
+
+// all band names get pushed into this array when addToArray() is called. we may not need to use this since I figured out how to store the entire object, but i left it in
+var bands = []
+
+// all venue lat and lons and event names are stored in these next two. same deal as above...might not need it now that i figured out how to store the whole object, but i left it in.
+var venueLatLon = []
+var venueNames = []
+
+// this variable is important, it stores the ENTIRE response from bands in town
+var bitResponse = ''
+
+// the following two variables will store the ENTIRE object for both artists and venues. all fields can be accessed through these. 
+var midstepArtists = []
+var midstepLocations = []
+var otherInfo = []
+
+// still working with this one, playing around with it, ignore it for now.
+var mapLongLat
 
 $.ajax({
-  url: "https://api.bandsintown.com/artists/metallica/events/recommended?location=new+york,NY&radius=10&app_id=RUCB&api_version=2.0&format=json",
+    url: "https://api.bandsintown.com/artists/nas/events/recommended?location=new+york,NY&radius=10&app_id=RUCB&api_version=2.0&format=json",
 
-  jsonp: "callback",
+    jsonp: "callback",
 
-  dataType: "jsonp",
+    dataType: "jsonp",
 
-  data: {
-    q: "name",
-    format: "json"
-  },
+    data: {
+        q: "name",
+        format: "json"
+    },
 
-  success: function(response) {
-    console.log(response); 
-    bitResponse = response;
-  },
+
+
+        success: function( response ) {
+        console.log(response); 
+        bitResponse = response;
+        addToArray();
+        addToTable();
+    }
 
 });
-function addToArray(){  
-  for(var i = 0; i < bitResponse.length; i++){
-    // pushes the name and lat long of the venues into an array stored in a global variable
-    venueNames.push(bitResponse[i].venue.name);
-    venueLatLon.push("lat:"+bitResponse[i].venue.latitude + " lon:" + bitResponse[i].venue.longitude);
-    // second for loop for the artist information
-    for(var j = 0; j < bitResponse[i].artist.length; j++ ){
-      // nate recommended this and helped me through it, but i am unsure of the use now that i have gotten further along, so I am commenting it out.             
-      midstep = bitResponse[i].artists[j];
-      bands.push(midstep.name);
-    };
-  };
-};
-addToArray();
 
-// Script to Activate the Carousel 
-  $('.carousel').carousel({
-    interval: 5000 //changes the speed
-  });
+      function addToArray(){  
+        for(var i = 0; i < bitResponse.length; i++){
+
+            // pushes the name and lat long of the venues into an array stored in a global variable
+
+           venueNames.push(bitResponse[i].venue.name);
+
+           midstepLocations.push(bitResponse[i].venue);
+
+           venueLatLon.push("lat:"+bitResponse[i].venue.latitude + " lon:" + bitResponse[i].venue.longitude);
+
+           //pushes all other info from object to the other info array
+           otherInfo.push(bitResponse[i]);
+
+           // second for loop for the artist information
+
+           for(var j = 0; j < bitResponse[i].artists.length; j++ ){
+
+            // after spending time trying to get the names to push into array, got it to work, but decided to push the entire artist object into an array for the global variable called "midstep"
+
+            midstepArtists.push(bitResponse[i].artists[j]);
+
+           bands.push(bitResponse[i].artists[j].name);
+
+           };
+    };
+};
+
+//code to add information and create the table
+
+function createTable(){
+  for(var i=0; i < bitResponse.length; i++){
+    var collapse = "collapse" + i;
+
+    var panelDefault = $('<div>');
+        panelDefault.addClass('panel panel-default')
+        panelDefault.addClass('linkAllOthers');
+        panelDefault.attr('id', collapse);
+        $('.addAll').append(panelDefault);
+
+    var panelHeading = $('<div>');
+        panelHeading.addClass('panel-heading');
+        panelHeading.attr('role', "tab");
+        panelHeading.attr('id', midstepArtists[i].name);
+        panelHeading.addClass("heading");
+        $('.linkAllOthers').append(panelHeading);
+
+    var panelTitle = $('<h4>');
+        panelTitle.addClass("panel-title");
+        panelTitle.addClass('title');
+        $('.heading').append(panelTitle);
+
+    var button = $('<a>');
+        // button.attr('id', "fourth");
+        button.attr('role', "button");
+        button.attr('data-toggle', 'collapse');
+        button.attr('data-parent', "#accordian");
+        button.attr('href', "#collapse"+[i]);
+        button.attr('aria-expanded', 'false');
+        button.attr('aria-controls', collapse);
+        button.text(otherInfo[i].title);
+        $('.title').append(button);
+
+    var tabPanel = $('<div>');
+        tabPanel.attr('id', collapse);
+        tabPanel.addClass('panel-collapse collapse in');
+        tabPanel.addClass('tab');
+        tabPanel.attr('role', 'tabpanel');
+        tabPanel.attr('aria-labelledby', '#'+ midstepArtists[i].name);
+        $('.linkAllOthers').append(tabPanel);
+
+    var panelBody = $('<div>');
+        panelBody.addClass('panel-body');
+        $('.tab').append(panelBody);
+
+    return false;
+    
+    console.log(venueNames[i]);
+    console.log(otherInfo[i].title);
+    console.log(midstepArtists[i].name);
+
+  }
+}
